d5b86c1f8e3b46eb2092bfbc9985d8ab0aa7c875
Yash Kumar <yk5708428@gmail.com>
2026-03-20 22:38:33 +0530

    Update API endpoint for fetching results

--- frontend/src/pages/admin/ManageResult.jsx
+++ frontend/src/pages/admin/ManageResult.jsx
@@ -9,7 +9,7 @@ const ManageResult = () => {
   useEffect(() => {
     const getResult = async () => {
       try {
-        const res = await axios.get("http://localhost:3000/course/allResult", {
+        const res = await axios.get("https://college-management-system-cpo2.onrender.com/course/allResult", {
           withCredentials: true,
         });
         setResults(res.data);
