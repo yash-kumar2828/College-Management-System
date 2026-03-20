8f875cdc949b6c093cb87e50a1654ae8caaddb5c
Yash Kumar <yk5708428@gmail.com>
2026-03-20 22:37:15 +0530

    Update API endpoint for fetching faculty data

--- frontend/src/pages/admin/ManageFaculty.jsx
+++ frontend/src/pages/admin/ManageFaculty.jsx
@@ -9,7 +9,7 @@ const ManageFaculty = () => {
       useEffect(()=>{
           const getFaculty=async()=>{
               try {
-                  const res=await axios.get("http://localhost:3000/faculty/allFaculty",{
+                  const res=await axios.get("https://college-management-system-cpo2.onrender.com/faculty/allFaculty",{
                       withCredentials:true,
                   });
                   console.log(res.data);
